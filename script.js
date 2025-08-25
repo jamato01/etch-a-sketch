3d90441593b71faa12e0a83bcaaaa6e8fb2f9b9a
jamato01 <jarrett.m.torres@gmail.com>
2025-08-25 14:46:16 -0400

    Add hover effect to grid using event listeners

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 // Create Grid
 
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
 for (i = 0; i < 16; i++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.setAttribute("class", "row-container");
+    const rowContainer = document.createElement('div');
+    rowContainer.setAttribute('class', 'row-container');
     container.appendChild(rowContainer);
     for (j = 0; j < 16; j++) {
-        const grid = document.createElement("div");
-        grid.setAttribute("class", "grid");
+        const grid = document.createElement('div');
+        grid.setAttribute('class', 'grid');
         rowContainer.appendChild(grid);
     }
 }
+
+// Add event listener for hover
+
+const grids = document.querySelectorAll('.grid');
+
+grids.forEach((element) => {
+    element.addEventListener('mouseover', (event) => {
+        element.classList.add('hovered');
+    });
+});
