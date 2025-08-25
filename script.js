66c7b7c1186067ea90b261ba86d1ca96f3db035a
jamato01 <jarrett.m.torres@gmail.com>
2025-08-25 15:27:33 -0400

    Add random rgb generator for hover effect

--- script.js
+++ script.js
@@ -40,7 +40,16 @@ function createNewGrid(gridSize) {
     const grids = document.querySelectorAll('.grid');
     grids.forEach((element) => {
         element.addEventListener('mouseover', (event) => {
-            element.classList.add('hovered');
+            element.style.backgroundColor = getRandomRGB();
         });
     });
+}
+
+// Extra Credit: Randomize RGB values
+
+function getRandomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`
 }
